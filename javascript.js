fbd7bc29ac73394a2248e5b988e1054b2b8b0141
Andrew Prinn <andrewprinn@gmail.com>
2022-10-14 14:28:11 -0400

    resize function with proper limits

--- javascript.js
+++ javascript.js
@@ -3,9 +3,18 @@ function colorFunction(e) {
     e.target.style.backgroundColor = "black";
 }
 
+const container = document.querySelector('#gridContainer');
+for (i = 0; i < (16*16); i++){
+    let cell = document.createElement('div');
+    cell.classList.add('cells');
+    cell.addEventListener("mouseover", colorFunction)
+    container.appendChild(cell);
+}
+
 function creategrid(){
     const container = document.querySelector('#gridContainer');
-    let j = prompt();
+    let j = prompt("Enter the number of squares per side for the new grid (limit 100)");
+    if (j < 100 && j > 0 && j % 1 == 0){
     for (i = 0; i < (j*j); i++){
     let cell = document.createElement('div');
     cell.classList.add('cells');
@@ -14,8 +23,12 @@ function creategrid(){
     let r = document.querySelector(':root');
     r.style.setProperty('--columnCount', j)
     }
+    }
+    else {
+        alert("That's....not right. Try it again. Between 0 and 100 this time. Whole numbers only.");
+    }
 }
-creategrid();
+
 
 function newGrid(){
     const cells = document.querySelectorAll('.cells');
