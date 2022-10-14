a3ad1a1e80468323a92fe40116945ee359a3ed0f
Andrew Prinn <andrewprinn@gmail.com>
2022-10-14 13:57:29 -0400

    dynamic grid size

--- javascript.js
+++ javascript.js
@@ -1,18 +1,26 @@
-const container = document.querySelector('#gridContainer');
-
 //draw black
 function colorFunction(e) {
     e.target.style.backgroundColor = "black";
 }
 
-//create grid
-let j = prompt("Choose the size of the drawing area (from 2-100)");
-for (i = 0; i < (j*j); i++){
-let cell = document.createElement('div');
-cell.classList.add('cells');
-cell.addEventListener("mouseover", colorFunction)
-container.appendChild(cell);
-let r = document.querySelector(':root');
-r.style.setProperty('--columnCount', j)
+function creategrid(){
+    const container = document.querySelector('#gridContainer');
+    let j = prompt();
+    for (i = 0; i < (j*j); i++){
+    let cell = document.createElement('div');
+    cell.classList.add('cells');
+    cell.addEventListener("mouseover", colorFunction)
+    container.appendChild(cell);
+    let r = document.querySelector(':root');
+    r.style.setProperty('--columnCount', j)
+    }
 }
+creategrid();
 
+function newGrid(){
+    const cells = document.querySelectorAll('.cells');
+    cells.forEach(cell => {
+        cell.remove();
+    });
+    creategrid();
+}
